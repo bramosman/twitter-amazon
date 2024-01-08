0c6d932d26caa8ddfc982703ae625ea3617bbd49
My namebram@theosmans.com <bram@upstager.co>
2024-01-08 16:18:35 -0500

    server debug

--- server/server.mjs
+++ server/server.mjs
@@ -3,13 +3,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import Twitter from 'twitter';
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Twitter API configuration
 const client = new Twitter({
   consumer_key: process.env.apiKey,
   consumer_secret: process.env.apiSecretKey,
@@ -17,10 +15,8 @@ const client = new Twitter({
   access_token_secret: process.env.accessTokenSecret,
 });
 
-// Define a route
 app.get('/tweets', (req, res) => {
-  // Your Twitter API logic here
-  const params = { screen_name: 'BramOsman4' }; // Replace with your Twitter screen name
+  const params = { screen_name: 'BramOsman4' };
   client.get('statuses/user_timeline', params, (error, tweets, response) => {
     if (!error) {
       res.json({ tweets });
@@ -30,7 +26,6 @@ app.get('/tweets', (req, res) => {
   });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
