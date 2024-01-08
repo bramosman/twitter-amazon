5f0b691a64d39992aec334edff2d7a07e9a5ea11
My namebram@theosmans.com <bram@upstager.co>
2024-01-08 16:40:01 -0500

    twitterAPI revert

--- src/api/twitterAPI.js
+++ src/api/twitterAPI.js
@@ -6,8 +6,7 @@ const BASE_URL = 'https://react-twitter-gray.vercel.app/tweets';
 export const getTweets = async (query) => {
   try {
     const response = await axios.get(`${BASE_URL}?query=${query}`);
-    console.log('Twitter API Response Status:', response.status);
-    console.log('Twitter API Response Data:', response.data);
+    console.log('Twitter API Response:', response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching tweets:', error.message);
