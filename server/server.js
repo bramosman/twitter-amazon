e75972bd07a19c9f67765831715ed165535e47a1
My namebram@theosmans.com <bram@upstager.co>
2024-01-08 17:02:57 -0500

    mjs update

--- server/server.js
+++ server/server.js
@@ -1,49 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+// server.js
+import express from 'express';
+import cors from 'cors';
+import { getTweets } from './api/twitterAPI.js';
+
 const app = express();
-const PORT = 5000;
 
-// Load environment variables
-require('dotenv').config();
-
-const { API_KEY, API_SECRET_KEY } = process.env;
-
-// Set CORS headers before defining routes
+// Enable CORS for all routes
 app.use(cors());
-app.use(express.json());
 
+// Define your API endpoint
 app.get('/tweets', async (req, res) => {
-  const { query } = req.query;
-
   try {
-    // Construct the bearer token
-    const bearerToken = Buffer.from(`${API_KEY}:${API_SECRET_KEY}`).toString('base64');
-
-    // Use the axios request with params directly
-    const twitterResponse = await axios.get('https://api.twitter.com/2/tweets/search/recent', {
-      headers: {
-        'Authorization': `Bearer ${bearerToken}`,
-        'Content-Type': 'application/json',
-      },
-      params: {
-        query: query,
-      },
-    });
-
-    res.json(twitterResponse.data);
+    const query = req.query.query || '#matterport';
+    const tweets = await getTweets(query);
+    res.json(tweets);
   } catch (error) {
-    console.error('Error fetching tweets from Twitter API:', error);
-
-    // Log more details about the error response from Twitter API
-    if (error.response) {
-      console.error('Twitter API Error Response:', error.response.data);
-    }
-
+    console.error('Error fetching tweets:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
