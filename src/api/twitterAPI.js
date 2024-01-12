56cd68b862b55dbf6d4fe0f9bdb8f151970d5a9e
My namebram@theosmans.com <bram@upstager.co>
2024-01-12 11:29:23 -0500

    handle html

--- src/api/twitterAPI.js
+++ src/api/twitterAPI.js
@@ -1,5 +1,6 @@
 // twitterAPI.js
 import axios from 'axios';
+import { load } from 'cheerio';
 
 const BASE_URL = 'https://react-twitter-gray.vercel.app';
 
@@ -14,29 +15,28 @@ export const getTweets = async (query) => {
 
     console.log('Twitter API Full Response:', response);
     console.log('Twitter API Response Status:', response.status);
-    
-    // Extract JSON data from HTML response
-    const jsonData = extractJsonFromHtml(response.data);
 
-    // Assuming the server returns an array directly
-    if (Array.isArray(jsonData)) {
-      return jsonData;
-    } else {
-      console.error('Invalid tweet data received.');
-      throw new Error('Invalid tweet data received.');
+    // Use the function returned by load to parse the HTML
+    const $ = load(response.data);
+    const scriptContent = $('script[id="__NEXT_DATA__"]').html();
+
+    try {
+      const jsonData = JSON.parse(scriptContent);
+      // Assuming the server returns an array directly
+      if (Array.isArray(jsonData)) {
+        return jsonData;
+      } else {
+        console.error('Invalid tweet data received.');
+        throw new Error('Invalid tweet data received.');
+      }
+    } catch (error) {
+      console.error('Error parsing JSON data:', error.message);
+      throw new Error('Error parsing JSON data.');
     }
   } catch (error) {
     console.error('Error fetching tweets:', error.message);
     throw new Error(`Failed to fetch tweets. Server response: ${error.message}`);
   }
-};
-
-// Function to extract JSON data from HTML
-const extractJsonFromHtml = (htmlData) => {
-  // Implement logic to extract JSON data from the HTML response
-  // For example, you can use a library like cheerio or regex
-  // Replace the following line with your implementation
-  return { data: 'Extracted JSON data from HTML' };
 };
 
 export default getTweets;
