0e41af29372542c2e57f6cee9d78294f0953a8d2
My namebram@theosmans.com <bram@upstager.co>
2024-01-15 12:21:37 -0500

    revert

--- src/api/twitterAPI.js
+++ src/api/twitterAPI.js
@@ -1,6 +1,4 @@
-// twitterAPI.js
 import axios from 'axios';
-import { load } from 'cheerio';
 
 const BASE_URL = 'https://react-twitter-gray.vercel.app';
 
@@ -9,36 +7,20 @@ export const getTweets = async (query) => {
     const response = await axios.get(`${BASE_URL}/tweets`, {
       params: { query },
       headers: {
-        'Accept': 'text/html', // Accept HTML response
+        'Accept': 'application/json', // Set the Accept header for JSON
       },
     });
 
     console.log('Twitter API Full Response:', response);
+    console.log('Twitter API Response Status:', response.status);
+    console.log('Twitter API Response Data:', response.data);
 
-    // Log the entire HTML response
-    console.log('HTML Response:', response.data);
-
-    // Use the function returned by load to parse the HTML
-    const $ = load(response.data);
-    const scriptContent = $('script[id="__NEXT_DATA__"]').html();
-
-    if (!scriptContent) {
-      console.error('No script content found in HTML.');
-      throw new Error('No script content found in HTML.');
-    }
-
-    try {
-      const jsonData = JSON.parse(scriptContent);
-      // Assuming the server returns an array directly
-      if (Array.isArray(jsonData)) {
-        return jsonData;
-      } else {
-        console.error('Invalid tweet data received.');
-        throw new Error('Invalid tweet data received.');
-      }
-    } catch (error) {
-      console.error('Error parsing JSON data:', error.message);
-      throw new Error('Error parsing JSON data.');
+    // Assuming the server returns an array directly
+    if (Array.isArray(response.data)) {
+      return response.data;
+    } else {
+      console.error('Invalid tweet data received.');
+      throw new Error('Invalid tweet data received.');
     }
   } catch (error) {
     console.error('Error fetching tweets:', error.message);
